a98cce7c3d9094ad8cee91be576d814dfe97627f
miko <ptechasa@mail.ccsf.edu>
2020-08-06 01:42:52 -0700

    fixed  Joi error

--- validation.js
+++ validation.js
@@ -4,21 +4,21 @@ const Joi = require('@hapi/joi');
 //Register Validation
 const registerValidation = data => {
     const schema = Joi.object({
-        name: Joi.string() .min(6) .required(),
-        email: Joi.string() .min(6) .required() .email(),
-        password: Joi.string() .min(6) .required()
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
     });
-    return Joi.validation(data, schema);
+    return Joi.assert(data, schema);
 };
 
 //Login Validation
 const loginValidation = data => {
     const schema = Joi.object({
-        name: Joi.string() .min(6) .required(),
-        email: Joi.string() .min(6) .required() .email(),
-        password: Joi.string() .min(6) .required()
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
     });
-    return Joi.validation(data, schema);
+    return Joi.assert(data, schema);
 };
 
 module.exports.registerValidation = registerValidation;
